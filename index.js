aa5b6ebb74084d42011069128d6462da9f187d8a
pedrogf84 <dev3.pgonzalez@gmail.com>
2024-01-19 17:45:00 +0100

    node app

--- index.js
+++ index.js
@@ -1,54 +1,66 @@
+// imports
+
 const express = require("express");
 require("dotenv").config();
-const app = express();
+const cors = require("cors");
 
-app.use((request, response, next) => {
+// config de la app
+const app = express();
+app.use(cors());
+
+// configurar la respuesta que da nuesta api node
+app.use((require, response, next) => {
   response.header("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE");
   response.header("Access-Control-Allow-Credentials", true);
   response.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (request, response) => {
+// rutas
+
+app.get("/", (request, response, next) => {
   response.status(200).json({
     status: 200,
     data: {
       method: "GET",
-      message: "Welcome to my server (GET)",
-      app: "My App",
+      message: "Bienvenido a la app (GET)",
     },
   });
 });
 
-app.post("/", (request, response) => {
+app.post("/", (request, response, next) => {
   response.status(200).json({
     status: 200,
     data: {
       method: "POST",
-      message: "Welcome to my server",
-      app: "My App",
+      message: "Bienvenido a la app (POST)",
     },
   });
 });
 
+app.disable("x-powered-by");
+
+// manejo de errores
+
 app.use((request, response, next) => {
   let error = new Error();
   error.status = 404;
-  error.message = HTTPSTATUSCODE[404];
+  error.message = "not found";
   next(error);
 });
 
 app.use((error, request, response, next) => {
-  return response
-    .status(error.status || 500)
-    .json(error.message || "Unexpected error");
+  return response.status(error.status || 500).json({
+    status: error.status || 500,
+    message: error.message || "Unexpected error",
+  });
 });
 
-app.disable("x-powered-by");
+// definir escucha de la api
 
-/* DEFINIR EL PUERTO E INICIAR LA ESCUCHA */
 app.listen(process.env.PORT, () => {
-  console.log(`app running in port ${process.env.PORT}`);
+  console.log(`app running on port ${process.env.PORT}`);
 });
